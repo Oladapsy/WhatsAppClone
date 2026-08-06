8faca6e0542e49ba857e63fb0e42a9d0539b08f5
oladapsy <dapoodedeyi@yahoo.com>
2026-08-06 16:53:39 +0100

    custom expo-env.d.ts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import WelcomeImage from "@/assets/images/home/welcome.png";
 
+// React Native handles static assets directly without resolveAssetSource
 export default function Index() {
   return (
     <View style={styles.container}>
+      {/* Pass the imported asset directly to the source prop */}
+      <Image source={WelcomeImage} style={{ width: 200, height: 200 }} />
+
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
@@ -13,5 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
   },
 });
