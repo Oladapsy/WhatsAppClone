1723fd1fe40d288b07ea9968efee9afeb3e5358b
oladapsy <dapoodedeyi@yahoo.com>
2026-08-06 16:38:23 +0100

    colors

--- src/shared/constants/colors.ts
+++ src/shared/constants/colors.ts
@@ -1,11 +1,25 @@
-export default {
+export const LightColors = {
   primary: '#1063FD',
   muted: '#3A5A92',
   background: '#EFEEF6',
+  text: '#1C1C1E',
   gray: '#6E6E73',
   lightGray: '#DCDCE2',
   green: '#4FEE57',
   lightGreen: '#DBFFCB',
   red: '#EF0827',
   yellow: '#FCC70B',
+};
+
+export const DarkColors = {
+  primary: '#1063FD',
+  muted: '#8A9FBF',
+  background: '#121212',
+  text: '#FFFFFF',
+  gray: '#98989D',
+  lightGray: '#2C2C2E',
+  green: '#4FEE57',
+  lightGreen: '#1C3D1E',
+  red: '#FF453A',
+  yellow: '#FFD60A',
 };
