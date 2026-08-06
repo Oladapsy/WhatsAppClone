b7354db75acc22a42245f5b711b82f6524c306ee
oladapsy <dapoodedeyi@yahoo.com>
2026-08-06 20:02:01 +0100

    feature: image welcome

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,4 @@
 import { Text, View, StyleSheet, Image } from "react-native";
-import WelcomeImage from "@/assets/images/home/welcome.png";
 import { LightColors } from "@/shared/constants/colors";
 
 // React Native handles static assets directly without resolveAssetSource
@@ -7,7 +6,10 @@ export default function Index() {
   return (
     <View style={styles.container}>
       {/* Pass the imported asset directly to the source prop */}
-      <Image source={WelcomeImage} style={styles.welcome} />
+      <Image
+        source={require("@/assets/images/home/welcome.png")}
+        style={styles.welcome}
+      />
 
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
@@ -26,7 +28,5 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 300,
     marginBottom: 80,
-
-
-  }
+  },
 });
